7ab8a1dc97fe82294a3a71de58c8eb544a28ede4
Marianna Papagiannopoulou <marianna.papagiannopoulou1@gmail.com>
2025-02-07 15:02:17 +0200

    cleanup

--- src/CSharpApp.Application/Products/Queries/Handlers/GetProductByIdQueryHandler.cs
+++ src/CSharpApp.Application/Products/Queries/Handlers/GetProductByIdQueryHandler.cs
@@ -1,42 +1,16 @@
 ﻿using CSharpApp.Application.Products.Queries;
-using CSharpApp.Core.Exceptions;
 using CSharpApp.Core.Interfaces;
-using Microsoft.Extensions.Logging;
 
 namespace CSharpApp.Application.Products.Handlers;
 
-public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, Product>
+public class GetProductByIdQueryHandler(
+	IProductsService productsService
+) : IRequestHandler<GetProductByIdQuery, Product?>
 {
-	private readonly IProductsService _productsService;
-	private readonly ILogger<GetProductByIdQueryHandler> _logger;
+	private readonly IProductsService _productsService = productsService;
 
-	public GetProductByIdQueryHandler(
-		IProductsService productsService,
-		ILogger<GetProductByIdQueryHandler> logger
-	)
-	{
-		_productsService = productsService;
-		_logger = logger;
-	}
-
-	public async Task<Product> Handle(
+	public async Task<Product?> Handle(
 		GetProductByIdQuery request,
 		CancellationToken cancellationToken
-	)
-	{
-		try
-		{
-			var product = await _productsService.GetProductById(request.Id, cancellationToken);
-
-			if(product is null)
-				throw new NotFoundException($"Product with ID {request.Id} was not found.");
-
-			return product;
-		}
-		catch(Exception ex)
-		{
-			_logger.LogError(ex, "An error occurred while getting the product by ID.");
-			throw;
-		}
-	}
+	) => await _productsService.GetProductById(request.Id, cancellationToken);
 }
